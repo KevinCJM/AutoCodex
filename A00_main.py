7164f7458bc3da8b6fb8790eaf2583467c5f766e
chenjm34827 <chenjm34827@hundsun.com>
2026-02-25 13:59:27 +0800

    需求分析增加 main

--- A00_main.py
+++ A00_main.py
@@ -6,10 +6,14 @@
 @Descriptions: 
 """
 
+from A01_requiment_analysis_workflow import main as requirement_workflow_main
 from A02_task_workflow import main as task_workflow_main
 from A03_coding_agent_workflow import main as coding_workflow_main
 
 if __name__ == "__main__":
+    # 需求分析 工作流
+    requirement_workflow_main()
+
     # 任务拆分 工作流
     task_workflow_main()
 
